50ae6233e7ff33672f87bca1d8109d5d061831c2
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-26 14:41:18 +0530

    chore : card rendering changes

--- client/src/components/CategoryCard.jsx
+++ client/src/components/CategoryCard.jsx
@@ -1,4 +1,20 @@
-import CategoryDisplay from "./CategoryDisplay.jsx";
+const CategoryDisplay = ({ imgSrc, name }) => {
+  return (
+    <div className="text-center">
+      <div className="w-[75px] h-[75px] rounded-full overflow-hidden bg-[#DFDCD7]">
+        <img
+          src={imgSrc}
+          alt="category"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="h-[4px]" />
+      <p className=" gilroy font-semibold line-clamp-2 text-xs text-center">
+        {name}
+      </p>
+    </div>
+  );
+};
 
 export default function CategoryCard() {
   return (
