98926dba513fef97715d907d9f91eb5f5b024590
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-26 12:01:18 +0530

    style (layout) : mino changes

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import Header from "./components/header";
 
 export default function Layout() {
   return (
--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Header() {
-  return <div className="w-[390px] h-[42px] bg-gray-400"></div>;
+  return <div className="w-[390px] h-[42px] bg-gray-400" />;
 }
