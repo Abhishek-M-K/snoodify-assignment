139f43415d3ac384a5cb4473a76defbd6b5a96a6
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-26 11:30:26 +0530

    chore : layout fixes

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/header";
-import Navigation from "./components/navigation";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 
 export default function Layout() {
   return (
